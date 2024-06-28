b8e750c10b1c9415da6bf42ed799fc75be013f33
Aliyumusamkk <Aliyumusakalakala@gmail.com>
2024-06-28 03:07:34 -0700

    initail commit

---
+++ vtu-app/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home banner-bg text-center text-color p-3">
+          <h1>Wellcome To Mufti Data_Sub</h1>
+          <p>Don't Ever Run Out Of Data & Airtime</p>
+      </div>
+      <section className="body">
+          <div className="container-lg">
+            <div className="row justify-content-center align-items-center py-lg-5">
+            <div className="col-md-5 text-center py-lg-5">
+              <img className='image img-fluid' src={require('./img/person.jfif')} alt="Images missed"/>
+              </div>
+              <div className="col-md-5 text-center text-md-start mb-3">
+                <h1>
+                  <div className="text-color1 fw-bold">Start Business</div>
+                  <div className="text-color2">Get cheap data And Airtime by starting using App</div>
+                </h1>
+                <a href="" className="btn btn-primary btn-style ">Get Started</a>
+              </div>
+            </div>
+          </div>
+      </section>
+
+{/* 
+      <div className="home-img">
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ vtu-app/src/components/Login.js
@@ -0,0 +1,15 @@
+// const Login = () => {
+//     return (
+//         <div className="log container" >
+//             <form action="">
+//             <div class="input-group mb-3">
+//                 <span class="input-group-text" id="basic-addon1">@</span>
+//                 <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+//                 aria-describedby="basic-addon1"></input>
+//         </div>
+//             </form>
+//         </div>
+//     );
+// }
+ 
+// export default Login;

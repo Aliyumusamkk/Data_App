4f28fe362ba6abb79984b5f1d8307b3f2c22c68d
Aliyumusamkk <Aliyumusakalakala@gmail.com>
2024-06-30 17:32:31 -0700

    new

--- vtu-app/src/index.js
+++ vtu-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import Loginform from './components/Loginform';
@@ -17,9 +17,9 @@ root.render(
       <div className="content">
         <Switch>
          
-          <Route path="/">
+          {/* <Route path="/">
           <App />
-          </Route>
+          </Route> */}
           
           <Route path="/Registrations">
           < Registration />
